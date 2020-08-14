4647126f521bfcff35ca5add05c2c8230a7e76d7
Kimon Froussios <jack_ohara_097@hotmail.com>
2020-08-14 19:05:59 +0200

    Fully integrate: from multiple BAMs to Rmd report

--- catch_workflow.sh
+++ catch_workflow.sh
@@ -11,7 +11,7 @@
 
 function usage() {
     echo "Usage:"
-    echo "      $0 -b BAM_FILE [-c DEMUX_BC] [-o OUTDIR] [-X PATH_TO_CATCH_SCRIPTS] [-m HAMMING_DIST] [-r] [-i] [-s]"
+    echo "      $0 -b BAM_DIR -o PROCESS_DIR -O REPORT_DIR -v COVARS [-A ABUND_THRESH] [-R REF_ROWS] [-c DEMUX_BC] [-m HAMMING_DIST] [-X PATH_TO_CATCH_SCRIPTS] [-r] [-i] [-s]"
     exit 1
 }
 
@@ -24,28 +24,32 @@ dxcnt=1
 dist=1
 post=1
 dark=0
+plot=1
+abund=0.01
+ref=1
 # Parse options.
-while getopts 'b:c:o:X:m:n:ris123' flag; do
+while getopts 'b:c:o:O:v:X:m:n:A:R:ris1234' flag; do
   case "${flag}" in
-    b) bam="${OPTARG}" ;;         # BAM file
+    b) bam="${OPTARG}" ;;         # BAM folder.
     c) barcodes="${OPTARG}" ;;    # Demultiplexing table (for all lengths of sample tags)
     o) outdir="${OPTARG}" ;;      # Output directory for the counts
+    O) resdir="${OPTARG}" ;;      # Ouptut directory for the analysis report
+    v) covars="${OPTARG}" ;;      # List of samples in desired order, corresponding condition, desired corresponding colour
     X) SCRIPTSPATH="${OPTARG}" ;; # Where to find all the scripts for this workflow
     m) hamm="${OPTARG}" ;;        # Hamming distance at which to merge barcodes as likely sequencing errors
     n) dark="${OPTARG}" ;;        # Number of dark bases to allow in the pattern (0)
+    A) abund="${OPTARG}" ;;       # Barcode abundance threshold for the report (0.01).
+    R) ref="${OPTARG}" ;;         # Comma seperated list of row numbers in covars to be used as reference samples in report, NOT counting the header line (1).
     r) revcomp="${OPTARG}" ;;     # Reverse complement the barcodes
     i) spikedin="${OPTARG}" ;;    # Spike-in barcode was added (hard-coded barcode sequence)
     s) stringent="${OPTARG}" ;;   # Match full format of semi-random barcodes
     1) dxcnt=0 ;;                 # Skip demux and count
     2) dist=0 ;;                  # Skip hammind distance merge
     3) post=0 ;;                  # Skip table mergers
+    4) plot=0 ;;                  # Skip analysis report.
     *) usage ;;
   esac
 done
-
-if [[ ! -z "$barcodes" ]]; then
-    barcodes="-b ${barcodes}"
-fi
 
 if [[ -z "$outdir" ]]; then
   outdir='./process'
@@ -55,27 +59,6 @@ if [[ ! -d $outdir ]]; then
   mkdir -p $outdir
 fi
 
-if [[ -z "$unknown" ]]; then
-  unknown='./unknown.fastq'
-fi
-
-if [ $revcomp ]; then
-  revcomp='-r'
-else
-  revcomp=''
-fi
-
-if [ $spikedin ]; then
-  spikedin='-i'
-else
-  spikedin=''
-fi
-
-if [ $stringent ]; then
-  stringent='-s'
-else
-  stringent=''
-fi
 
 wait_for_jobs(){
   echo "waiting"
@@ -114,10 +97,32 @@ prefix=${prefix/.bam/}
 
 
 if [ "$dxcnt" -eq 1 ]; then
-  # Match semi-random barcode format, demultiplex, and count the barcodes.
-  echo "${prefix}: Demultiplexing and counting barcodes"
-  sbatch -J CaTCHc --mem=50G -o /dev/null -e /dev/null ${SCRIPTSPATH}/barcodingQuantifier.py -f $bam -o $outdir --n_dark $dark $barcodes $revcomp $spikedin $stringent
-  wait_for_jobs CaTCHc
+    if [[ ! -z "$barcodes" ]]; then
+        barcodes=",-b ${barcodes}"
+    fi
+
+    if [ $revcomp ]; then
+      revcomp=',-r'
+    else
+      revcomp=''
+    fi
+
+    if [ $spikedin ]; then
+      spikedin=',-i'
+    else
+      spikedin=''
+    fi
+
+    if [ $stringent ]; then
+      stringent=',-s'
+    else
+      stringent=''
+    fi
+
+    # Match semi-random barcode format, demultiplex, and count the barcodes.
+    echo "${prefix}: Demultiplexing and counting barcodes"
+    ${SCRIPTSPATH}/fileutilities.py T $bam --dir 'bam$' | ${SCRIPTSPATH}/fileutilities.py P --loop sbatch ,-J CaTCHc ,--mem=50G ,-o /dev/null ,-e /dev/null ${SCRIPTSPATH}/barcodingQuantifier.py ,-f {abs} ,-o $outdir ,--n_dark $dark $barcodes $revcomp $spikedin $stringent
+    wait_for_jobs CaTCHc
 fi
 
 if [ "$dist" -eq 1 ]; then
@@ -125,24 +130,31 @@ if [ "$dist" -eq 1 ]; then
   if [ "$hamm" -gt 0 ]; then
       echo "${prefix}: Merging similar barcodes at distance $hamm"
       ## preserve unmerged counts in a new name, so the merged can have the name that is used downstream
-      ${SCRIPTSPATH}/fileutilities.py T ${outdir}/${prefix} --dir barcode-counts.txt | ${SCRIPTSPATH}/fileutilities.py P --loop mv {abs} {dir}/{bas}.raw.txt
-      ${SCRIPTSPATH}/fileutilities.py T ${outdir}/${prefix} --dir barcode-counts.raw.txt | ${SCRIPTSPATH}/fileutilities.py P --loop sbatch ,-o /dev/null ,-e /dev/null ,-J CaTCHh ,--qos=medium ,--mem=20G ${SCRIPTSPATH}/barcodingHammingMerge.py ,-b {abs} ,-d $hamm ,-o {dir}/{cor}.txt
-      # ${SCRIPTSPATH}/fileutilities.py T ${outdir}/${prefix} --dir barcode-counts.raw.txt | ${SCRIPTSPATH}/fileutilities.py P --loop ${SCRIPTSPATH}/barcodingHammingMerge.py ,-b {abs} ,-d $hamm ,-o {dir}/{cor}.txt
+      ${SCRIPTSPATH}/fileutilities.py T ${outdir}/*/ --dir barcode-counts.txt | ${SCRIPTSPATH}/fileutilities.py P --loop mv {abs} {dir}/{bas}.raw.txt
+      ${SCRIPTSPATH}/fileutilities.py T ${outdir}/*/ --dir barcode-counts.raw.txt | ${SCRIPTSPATH}/fileutilities.py P --loop sbatch ,-o /dev/null ,-e /dev/null ,-J CaTCHh ,--qos=medium ,--mem=20G ${SCRIPTSPATH}/barcodingHammingMerge.py ,-b {abs} ,-d $hamm ,-o {dir}/{cor}.txt
       wait_for_jobs CaTCHh
   fi
 fi
 
 if [ "$post" -eq 1 ]; then
-  # Merge sample-specific count tables into one collective table
   echo "${prefix}: Merging sample-specific count tables into one"
-  ${SCRIPTSPATH}/mergeBCcounts.R ${outdir}/${prefix} ${outdir}/${prefix}_barcode-counts.tsv _barcode-counts.txt
+  # Merge sample-specific count tables into one collective table
+  ${SCRIPTSPATH}/mergeBCcounts.R ${outdir}/${prefix}_barcode-counts.txt ${outdir}/*/*_barcode-counts.txt
 
   # Also merge the summary reports.
-  ${SCRIPTSPATH}/mergeBCcounts.R ${outdir}/${prefix} ${outdir}/${prefix}_summary.tsv _summary.txt
+  ${SCRIPTSPATH}/mergeBCcounts.R ${outdir}/${prefix}_summaries.txt ${outdir}/*/*_summary.txt > /dev/null   # The stdout merge report is not sensible for the summaries file.
 fi
 
-# # Note to self how to merge the hammdist tables.  
+# # Note to self how to merge the hammdist tables.
 # head -n 2 ${outdir}/${prefix}/*hammdist.txt | tail -n 1 > ${outdir}/${prefix}_hammdist.tsv
 # tail -n +3 ${outdir}/${prefix}/*hammdist.txt | perl -e 'while($line=<STDIN>){print $line if $line !~/^[=\n]/}' >> ${outdir}/${prefix}_hammdist.tsv
+
+
+if [ "$plot" -eq 1 ]; then
+  echo "${prefix}: Compiling barcoding report"
+  srun --mem=9G ${SCRIPTSPATH}/barcoding_results_run.R -c ${outdir}/${prefix}_barcode-counts.txt -s ${outdir}/${prefix}_summaries.txt -d ${resdir}/${prefix} -v $covars -r $ref -A $abund
+fi
+
+
 
 echo "${prefix}: Workflow finished!"
--- mergeBCcounts.R
+++ mergeBCcounts.R
@@ -3,16 +3,15 @@
 library(data.table)
 
 args <- commandArgs(trailingOnly = TRUE)
-# args <- c('/Volumes/groups/obenauf/Kimon_Froussios/chris/CE83TANXX_review/process/CE83TANXX_7_20200221B_20200224', 'test.txt', '_barcode-counts.txt')
+# args <- c('/Volumes/groups/obenauf/Kimon_Froussios/shona/catch_R9739/testpro/barcode-counts.txt', '/Volumes/groups/obenauf/Kimon_Froussios/shona/catch_R9739/testpro/000000000-G5Y7J_1_20200603B_20200604/day0_barcode-counts.txt', '/Volumes/groups/obenauf/Kimon_Froussios/shona/catch_R9739/testpro/000000000-G5Y7J_1_20200603B_20200604/L11_treat_barcode-counts.txt')
 
-patt <-  args[3]
-files <- dir(args[1], pattern=patt, full.names=TRUE)
-out <- args[2]
+out <- args[1]
+files <- args[2:length(args)]
 
-# f <- files[1]
 tables <- lapply(files, function(f) {
+	# f <- files[1]
 	DT <- fread(f)
-	names(DT) <- c(ifelse(grepl('barcode', patt), 'barcode', 'sample'), sub(patt, '', basename(f)))
+	names(DT) <- c(ifelse(grepl('barcode', f), 'barcode', 'sample'), sub('_barcode-counts.txt|summary.txt', '', basename(f)))
 	DT
 })
 
@@ -26,8 +25,8 @@ print( colSums(counts[, names(counts) != id, with=FALSE]) )
 
 # On-screen report of barcode counts
 print("Barcode Counts")
-vapply(counts[, names(counts) != id, with=FALSE], 
+print(vapply(counts[, names(counts) != id, with=FALSE], 
 			 function(x) { sum(x>0) }, 
-			 integer(1) )
+			 integer(1) ) )
 
 fwrite(counts, file=out, quote=FALSE, col.names=TRUE, row.names=FALSE, sep="\t")
